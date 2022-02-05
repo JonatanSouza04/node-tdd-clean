5204ec4557fbc714e1d8d2031f03bdfdb3dfb46c
Jonatan Souza <jonatan.souza04@gmail.com>
2022-02-05 11:08:15 -0300

    chore: change jest config prettier

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const config = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  // coverageProvider: 'v8',
+  roots: [
+    '<rootDir>/src'
+  ],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
+
+module.exports = config
