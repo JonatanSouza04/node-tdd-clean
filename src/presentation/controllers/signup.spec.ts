847bb8106295de2e97854dc2e05c28bb5d56dd2d
Jonatan Souza <jonatan.souza04@gmail.com>
2022-02-05 12:34:58 -0300

    test: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,11 +1,10 @@
 import { SignUpController } from './signup';
 
 describe('SignUp Controller', () => {
-  test('Should return 100 if name is provider', () => {
+  test('Should return 100 if name is provided', () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
-        name: 'Jonatan Souza',
         email: 'jonatan@gmail.com',
         password: '123456',
         passwordConfirm: '123456',
@@ -15,4 +14,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
+
+  test('Should return 100 if no email is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'Jonatan Souza',
+        password: '123456',
+        passwordConfirm: '123456',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+  });
+
+
 });
