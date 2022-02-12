75a4f84f09d8a4bfa3c599746588163be06b6833
Jonatan Souza <jonatan.souza04@gmail.com>
2022-02-12 18:05:21 -0300

    test: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,17 +1,18 @@
 import { MongoHelper } from '../helpers/mongo-helper';
+import { AccountMongoRepository } from './account';
 
 describe('Account Mongo Repository', () => {
   beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL);
+    await MongoHelper.connect(process.env.MONGO_URL as string);
   });
 
   afterAll(async () => {
     await MongoHelper.disconnect();
   });
 
-  test('Should return an account on sucess', () => {
+  test('Should return an account on sucess', async () => {
     const sut = new AccountMongoRepository();
-    const account = sut.add({
+    const account = await sut.add({
       name: 'any_name',
       email: 'any_email@gmail.com',
       password: 'any_password',
