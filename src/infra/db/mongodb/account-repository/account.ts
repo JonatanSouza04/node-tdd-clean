0c3498d9457c580a47f88691e015163473abfe18
Jonatan Souza <jonatan.souza04@gmail.com>
2022-02-19 11:10:04 -0300

    feat: snsure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -8,7 +8,7 @@ import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts');
+    const accountCollection = await MongoHelper.getCollection('accounts');
     const accountToBeInserted = Object.assign({}, accountData);
     const result = await accountCollection.insertOne(accountToBeInserted);
     return MongoHelper.map(accountData, result.insertedId.toString());
