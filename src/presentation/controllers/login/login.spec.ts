6e60c0b0b7e80989c3734a1634afc18b7a8fadad
Jonatan Souza <jonatan.souza04@gmail.com>
2022-02-28 17:20:38 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,9 +2,21 @@ import { LoginController } from './login';
 import { badRequest } from '../../helpers/http-helper';
 import { MissingParamError } from '../../erros';
 
+interface SutTypes {
+  sut: LoginController;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController();
+
+  return {
+    sut,
+  };
+};
+
 describe('Login Controller', () => {
-  test('Should return 400 if no email is providede', async () => {
-    const sut = new LoginController();
+  test('Should return 400 if no email is provided', async () => {
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         password: 'any_password',
@@ -12,5 +24,16 @@ describe('Login Controller', () => {
     };
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')));
+  });
+
+  test('Should return 400 if no password is provided', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
   });
 });
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -4,6 +4,14 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 
 export class LoginController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const requiredFields = ['email', 'password'];
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
+
     return await new Promise((resolve) =>
       resolve(badRequest(new MissingParamError('email'))),
     );
