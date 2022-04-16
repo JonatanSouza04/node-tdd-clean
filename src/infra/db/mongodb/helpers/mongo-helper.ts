33edad23db0375b489f50c1e03dbf77878255b31
Jonatan Souza <jonatan.souza04@gmail.com>
2022-04-16 17:14:35 -0300

    fix: correct test MongoHelper and ServerError

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,13 +4,13 @@ export const MongoHelper = {
   client: null as unknown as MongoClient,
   uri: null as unknown as string,
 
-  async connect(uri = ''): Promise<void> {
+  async connect(uri): Promise<void> {
     this.uri = uri;
     this.client = await MongoClient.connect(uri);
   },
 
   async disconnect(): Promise<void> {
-    this.client?.close();
+    this.client.close();
     this.client = null;
   },
 
--- src/presentation/erros/server-error.ts
+++ src/presentation/erros/server-error.ts
@@ -1,5 +1,5 @@
 export class ServerError extends Error {
-  constructor(stack = '') {
+  constructor(stack) {
     super('Internal Server Error');
     this.name = 'ServerParamError';
     if (stack !== '') this.stack = stack;
