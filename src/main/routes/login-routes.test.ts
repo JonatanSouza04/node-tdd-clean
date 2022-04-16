c50054e33718e1434cd17d9a001447087f47de8c
Jonatan Souza <jonatan.souza04@gmail.com>
2022-04-16 16:49:19 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
+import { Collection } from 'mongodb';
+import { hash } from 'bcrypt';
+
+let accountColletion: Collection;
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   });
 
   beforeEach(async () => {
-    const accountColletion = await MongoHelper.getCollection('accounts');
+    accountColletion = await MongoHelper.getCollection('accounts');
     await accountColletion.deleteMany({});
   });
 
@@ -25,6 +29,26 @@ describe('Login Routes', () => {
           email: 'valid_email@gmail.com',
           password: '1234',
           passwordConfirm: '1234',
+        })
+        .expect(200);
+    });
+  });
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const hashPassword = await hash('1234', 12);
+
+      await accountColletion.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: hashPassword,
+      });
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'any_email@mail.com',
+          password: '1234',
         })
         .expect(200);
     });
