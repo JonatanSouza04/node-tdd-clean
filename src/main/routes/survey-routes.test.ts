3711681cbc22f56eab5324bf2ef7ce08af7db770
Jonatan Souza <jonatan.souza04@gmail.com>
2022-07-18 16:40:03 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,38 @@
+import request from 'supertest';
+import app from '../config/app';
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
+import { Collection } from 'mongodb';
+
+let surveyColletion: Collection;
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    surveyColletion = await MongoHelper.getCollection('surveys');
+    await surveyColletion.deleteMany({});
+  });
+
+  describe('POST /signup', () => {
+    test('Should return 204 on add survey success', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'any_question',
+          answers: [
+            {
+              answer: 'any_answer',
+              image: 'any_image',
+            },
+          ],
+        })
+        .expect(204);
+    });
+  });
+});
