28599a631b1fe1ced12ddf90f2d2084c3568ca7a
Jonatan Souza <jonatan.souza04@gmail.com>
2022-07-19 13:47:23 -0300

    test: ensure AuthMiddleware returns 500 if LoadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,5 +1,5 @@
 import { HttpRequest } from '../protocols';
-import { forbidden, ok } from '../helpers/http/http-helper';
+import { forbidden, ok, serverError } from '../helpers/http/http-helper';
 import { ForbiddenError } from '../erros';
 import { AuthMiddleware } from './auth-middleware';
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token';
@@ -71,5 +71,18 @@ describe('Auth Middleware', () => {
     const { sut } = makeSut();
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(ok({ accountId: 'valid_id' }));
+  });
+
+  test('Should return 500 if LoadAccountByToken throws', async () => {
+    const { sut, loadAccountByToken } = makeSut();
+    jest
+      .spyOn(loadAccountByToken, 'load')
+      .mockReturnValueOnce(
+        new Promise((resolve) =>
+          resolve(new Promise((resolve, reject) => reject(new Error()))),
+        ),
+      );
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
