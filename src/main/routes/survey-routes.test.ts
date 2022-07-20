54854b4ff74cf1225b113845c5bf50f2959a5ba6
Jonatan Souza <jonatan.souza04@gmail.com>
2022-07-20 15:43:10 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,12 +2,15 @@ import request from 'supertest';
 import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import { Collection } from 'mongodb';
+import { sign } from 'jsonwebtoken';
+import env from '../config/env';
 
 let surveyColletion: Collection;
+let accountColletion: Collection;
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL);
+    await MongoHelper.connect(env.mongoURL);
   });
 
   afterAll(async () => {
@@ -17,6 +20,9 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyColletion = await MongoHelper.getCollection('surveys');
     await surveyColletion.deleteMany({});
+
+    accountColletion = await MongoHelper.getCollection('accounts');
+    await accountColletion.deleteMany({});
   });
 
   describe('POST /signup', () => {
@@ -33,6 +39,41 @@ describe('Survey Routes', () => {
           ],
         })
         .expect(403);
+    });
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountColletion.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: '123',
+        role: 'admin',
+      });
+
+      const id = res.insertedId.toString();
+      const accessToken = sign({ id }, env.jwtSecret);
+
+      await accountColletion.updateOne(
+        { _id: MongoHelper.objectID(id) },
+        {
+          $set: {
+            accessToken,
+          },
+        },
+      );
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [
+            {
+              answer: 'any_answer',
+              image: 'any_image',
+            },
+          ],
+        })
+        .expect(204);
     });
   });
 });
