8034bcfc1112adde6aa95f60c221840e4da6c949
Jonatan Souza <jonatan.souza04@gmail.com>
2022-08-01 14:23:59 -0300

    fix: make controller return the error message instead of the error name

--- src/presentation/erros/forbidden-error.ts
+++ src/presentation/erros/forbidden-error.ts
@@ -1,7 +1,6 @@
 export class ForbiddenError extends Error {
   constructor(stack: string) {
-    super('Access denied (ForbiddenError)');
-    this.name = 'ServerParamError';
-    if (stack !== '') this.stack = stack;
+    super(stack);
+    this.name = 'ForbiddenError';
   }
 }
