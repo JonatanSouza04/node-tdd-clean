7aa6e953f8da02f85df688095e6d3b149fe2fab8
Jonatan Souza <jonatan.souza04@gmail.com>
2022-08-02 12:00:45 -0300

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with values correct

---
+++ src/data/protocols/db/survey/save-survey-result-repository.ts
@@ -0,0 +1,8 @@
+import {
+  SaveSurveyResultModel,
+  SurveyResultModel,
+} from '@/domain/models/survey-result';
+
+export interface SaveSurveyResultRepository {
+  save: (data: SaveSurveyResultModel) => Promise<SurveyResultModel>;
+}
---
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -0,0 +1,17 @@
+import {
+  SaveSurveyResultRepository,
+  SaveSurveyResultModel,
+  SurveyResultModel,
+  SaveSurveyResult,
+} from './db-save-survey-result-protocols';
+
+export class DbSaveSurveyResult implements SaveSurveyResult {
+  constructor(
+    private readonly saveSurveyResultRepository: SaveSurveyResultRepository,
+  ) {}
+
+  async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const survey = await this.saveSurveyResultRepository.save(data);
+    return survey;
+  }
+}
