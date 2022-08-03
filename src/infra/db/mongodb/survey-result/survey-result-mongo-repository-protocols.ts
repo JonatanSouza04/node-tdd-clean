e5c05a337ea18a2ab781d5a91a77f0afe8daaee4
Jonatan Souza <jonatan.souza04@gmail.com>
2022-08-03 16:23:11 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if there is new

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository-protocols.ts
@@ -0,0 +1,4 @@
+export * from '@/data/protocols/db/survey/save-survey-result-repository';
+export * from '@/data/protocols/db/survey/load-survey-by-id-repository';
+export * from '@/domain/usecases/save-survey-result';
+export * from '@/domain/models/survey-result';
---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,32 @@
+import { SaveSurveyResultRepository } from './survey-result-mongo-repository-protocols';
+import {
+  SaveSurveyResultModel,
+  SurveyResultModel,
+} from '@/domain/models/survey-result';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save(data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultColletion = await MongoHelper.getCollection(
+      'surveyResults',
+    );
+    const result = await surveyResultColletion.findOneAndUpdate(
+      {
+        surveyId: data.surveyId,
+        accountId: data.accountId,
+      },
+      {
+        $set: {
+          answer: data.answer,
+          date: data.date,
+        },
+      },
+      {
+        upsert: true,
+        returnDocument: 'after',
+      },
+    );
+
+    return MongoHelper.map(data, result.value._id.toString());
+  }
+}
