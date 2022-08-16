e68e9479056da693a110f5f0aefbf90155532fc5
Jonatan Souza <jonatan.souza04@gmail.com>
2022-08-16 20:10:44 -0300

    test: ensure SaveSurveyResultController returns 200 on success

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -11,6 +11,7 @@ import mockDate from 'mockdate';
 import {
   forbidden,
   serverError,
+  ok,
 } from '@/presentation/helpers/http/http-helper';
 import { InvalidParamError } from '@/presentation/erros';
 
@@ -159,5 +160,11 @@ describe('SaveSurveyResult Controller', () => {
 
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(ok(makeFakeSaveSurveyResult()));
   });
 });
