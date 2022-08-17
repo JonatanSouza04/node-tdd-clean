a213b2a45e500beaeca6d29fae78b107fdab65e5
Jonatan Souza <jonatan.souza04@gmail.com>
2022-08-17 20:35:34 -0300

    feat: ensure save survey result route return 403 if no token is provided

---
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
@@ -0,0 +1,14 @@
+import { Controller } from '@/presentation/protocols';
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory';
+import { SaveSurveyResultController } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller';
+import { makeDbLoadSurveyById } from '@/main/factories/usecases/survey-result/load-survey-by-id/db-load-survey-by-id-factory';
+import { makeDbSaveSurveyResult } from '@/main/factories/usecases/survey-result/save-survey-result/db-save-surveys-result-factory';
+
+export const makeSaveSurveyResultController = (): Controller => {
+  return makeLogControllerDecorator(
+    new SaveSurveyResultController(
+      makeDbLoadSurveyById(),
+      makeDbSaveSurveyResult(),
+    ),
+  );
+};
