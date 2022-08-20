6bd2309fb70da3c5dec09a6116875853683e0d50
Jonatan Souza <jonatan.souza04@gmail.com>
2022-08-20 14:25:59 -0300

    fix: fix imports

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository-protocols.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository-protocols.ts
@@ -1,4 +1,4 @@
 export * from '@/data/protocols/db/survey-result/save-survey-result/save-survey-result-repository';
 export * from '@/data/protocols/db/survey/load-survey-by-id/load-survey-by-id-repository';
-export * from '@/domain/usecases/save-survey-result';
+export * from '@/domain/usecases/survey-result/save-survey-result';
 export * from '@/domain/models/survey-result';
--- src/infra/db/mongodb/survey/survey-mongo-repository-protocols.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository-protocols.ts
@@ -1,5 +1,5 @@
 export * from '@/data/protocols/db/survey/add-survey/add-survey-repository';
 export * from '@/data/protocols/db/survey/load-survey-by-id/load-survey-by-id-repository';
 export * from '@/data/protocols/db/survey/load-surveys/load-surveys-repository';
-export * from '@/domain/usecases/add-survey';
+export * from '@/domain/usecases/survey/add-survey';
 export * from '@/domain/models/survey';
